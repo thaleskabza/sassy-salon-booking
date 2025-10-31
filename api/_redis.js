26b3f3fb634634204517d5ae389293576873c259
KABELO MODIPA <kabelo.modipa@codicisqa.com>
2025-10-31 23:43:41 +0200

    Change Redis connection URL for testing purposes
    Updated Redis client connection URL to use a PostgreSQL connection string for testing.

--- api/_redis.js
+++ api/_redis.js
@@ -3,9 +3,9 @@ import { createClient } from 'redis';
 import dotenv from 'dotenv';
 
 dotenv.config(); // Load from .env
-console.log('REDIS_URL:', process.env.REDIS_URL); // remove after testing
+console.log('REDIS_URL:', psql 'postgresql://neondb_owner:npg_GITDjUN6YK8V@ep-hidden-wave-a48c3cmv-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require'); // remove after testing
 
-const redis = createClient({ url: process.env.REDIS_URL });
+const redis = createClient({ url: psql 'postgresql://neondb_owner:npg_GITDjUN6YK8V@ep-hidden-wave-a48c3cmv-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require' });
 
 redis.on('error', err => console.error('Redis Client Error', err));
 
